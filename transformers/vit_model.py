ae7d896848eefc026b71fd83aaddf1b83c03e05a
maochengxu <43055265+maochengxu@users.noreply.github.com>
2022-12-21 21:27:40 +0000

    Update vit

--- transformers/vit_model.py
+++ transformers/vit_model.py
@@ -13,47 +13,45 @@ import torchvision
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 
+
 def img_to_patch(x, patch_size, flatten_channels=True):
     """
-    Inputs:
-        x - torch.Tensor representing the image of shape [B, C, H, W]
-        patch_size - Number of pixels per dimension of the patches (integer)
-        flatten_channels - If True, the patches will be returned in a flattened format
-                           as a feature vector instead of a image grid.
+    x is a torch.Tensor representing an image with dimensions [Batch size, Number of channels, Height, Width].
+    The patch_size is the number of pixels per dimension of the patches
+    flatten_channels is a boolean indicating whether the patches should be returned
+    as a flattened feature vector or as an image grid."
     """
     B, C, H, W = x.shape
-    x = x.reshape(B, C, H//patch_size, patch_size, W//patch_size, patch_size)
-    x = x.permute(0, 2, 4, 1, 3, 5) # [B, H', W', C, p_H, p_W]
-    x = x.flatten(1,2)              # [B, H'*W', C, p_H, p_W]
+    x = x.reshape(B, C, H // patch_size, patch_size, W // patch_size, patch_size)
+    # [B, H', W', C, p_H, p_W] then [B, H'*W', C, p_H, p_W]
+    x = x.permute(0, 2, 4, 1, 3, 5).flatten(1, 2)
     if flatten_channels:
-        x = x.flatten(2,4)          # [B, H'*W', C*p_H*p_W]
+        # [B, H'*W', C*p_H*p_W]
+        x = x.flatten(2, 4)
     return x
 
-class AttentionBlock(nn.Module):
 
+class AttentionBlock(nn.Module):
     def __init__(self, embed_dim, hidden_dim, num_heads, dropout=0.0):
         """
         Inputs:
-            embed_dim - Dimensionality of input and attention feature vectors
-            hidden_dim - Dimensionality of hidden layer in feed-forward network
-                         (usually 2-4x larger than embed_dim)
-            num_heads - Number of heads to use in the Multi-Head Attention block
-            dropout - Amount of dropout to apply in the feed-forward network
+            The embed_dim is the dimensionality of the input and attention feature vectors.
+            The hidden_dim is the dimensionality of the hidden layer in the feed-forward network,
+            The num_heads is the number of heads used in the Multi-Head Attention block,
+            the dropout is the amount of dropout applied in the feed-forward network
         """
         super().__init__()
 
         self.layer_norm_1 = nn.LayerNorm(embed_dim)
-        self.attn = nn.MultiheadAttention(embed_dim, num_heads,
-                                          dropout=dropout)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
         self.layer_norm_2 = nn.LayerNorm(embed_dim)
         self.linear = nn.Sequential(
             nn.Linear(embed_dim, hidden_dim),
             nn.GELU(),
             nn.Dropout(dropout),
             nn.Linear(hidden_dim, embed_dim),
-            nn.Dropout(dropout)
+            nn.Dropout(dropout),
         )
-
 
     def forward(self, x):
         inp_x = self.layer_norm_1(x)
@@ -61,9 +59,20 @@ class AttentionBlock(nn.Module):
         x = x + self.linear(self.layer_norm_2(x))
         return x
 
-class VisionTransformer(nn.Module):
 
-    def __init__(self, embed_dim, hidden_dim, num_channels, num_heads, num_layers, num_classes, patch_size, num_patches, dropout=0.0):
+class VisionTransformer(nn.Module):
+    def __init__(
+        self,
+        embed_dim,
+        hidden_dim,
+        num_channels,
+        num_heads,
+        num_layers,
+        num_classes,
+        patch_size,
+        num_patches,
+        dropout=0.0,
+    ):
         """
         Inputs:
             embed_dim - Dimensionality of the input feature vectors to the Transformer
@@ -82,38 +91,40 @@ class VisionTransformer(nn.Module):
 
         self.patch_size = patch_size
 
-        # Layers/Networks
-        self.input_layer = nn.Linear(num_channels*(patch_size**2), embed_dim)
-        self.transformer = nn.Sequential(*[AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)])
+        # define layers and nets
+        self.input_layer = nn.Linear(num_channels * (patch_size**2), embed_dim)
+        self.transformer = nn.Sequential(
+            *[
+                AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout)
+                for _ in range(num_layers)
+            ]
+        )
         self.mlp_head = nn.Sequential(
-            nn.LayerNorm(embed_dim),
-            nn.Linear(embed_dim, num_classes)
+            nn.LayerNorm(embed_dim), nn.Linear(embed_dim, num_classes)
         )
         self.dropout = nn.Dropout(dropout)
 
         # Parameters/Embeddings
-        self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
-        self.pos_embedding = nn.Parameter(torch.randn(1,1+num_patches,embed_dim))
-
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+        self.pos_embedding = nn.Parameter(torch.randn(1, 1 + num_patches, embed_dim))
 
     def forward(self, x):
-        # Preprocess input
+        # input preprocessing
         x = img_to_patch(x, self.patch_size)
         B, T, _ = x.shape
         x = self.input_layer(x)
 
-        # Add CLS token and positional encoding
+        # CLS token and positional encoding are added
         cls_token = self.cls_token.repeat(B, 1, 1)
         x = torch.cat([cls_token, x], dim=1)
-        x = x + self.pos_embedding[:,:T+1]
+        x = x + self.pos_embedding[:, : T + 1]
 
-        # Apply Transforrmer
+        # transformer application
         x = self.dropout(x)
         x = x.transpose(0, 1)
         x = self.transformer(x)
 
-        # Perform classification prediction
+        # classification prediction
         cls = x[0]
         out = self.mlp_head(cls)
         return out
-
